4aaca9a0881afbf9792902a77d9ea210d0f85ac9
giovannylemay <giovanny.lemay@epsi.fr>
2016-11-24 20:32:58 +0100

    Edit and add views

--- src/PlatformBundle/Controller/AdvertController.php
+++ src/PlatformBundle/Controller/AdvertController.php
@@ -22,16 +22,46 @@ class AdvertController extends Controller
         }
 
         // Ici, on récupérera la liste des annonces, puis on la passera au template
+        // Notre liste d'annonce en dur
+        $listAdverts = array(
+            array(
+                'title'   => 'Recherche développpeur Symfony',
+                'id'      => 1,
+                'author'  => 'Alexandre',
+                'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Mission de webmaster',
+                'id'      => 2,
+                'author'  => 'Hugo',
+                'content' => 'Nous recherchons un webmaster capable de maintenir notre site internet. Blabla…',
+                'date'    => new \Datetime()),
+            array(
+                'title'   => 'Offre de stage webdesigner',
+                'id'      => 3,
+                'author'  => 'Mathieu',
+                'content' => 'Nous proposons un poste pour webdesigner. Blabla…',
+                'date'    => new \Datetime())
+        );
+
 
         // Mais pour l'instant, on ne fait qu'appeler le template
-        return $this->render('PlatformBundle:Advert:index.html.twig');
-    }
+        return $this->render('PlatformBundle:Advert:index.html.twig', array(
+            'listAdverts' => $listAdverts));
+        }
 
     public function viewAction($id)
     {
+        $advert = array(
+          'title'   => 'Recherche développpeur Symfony2',
+          'id'      => $id,
+          'author'  => 'Alexandre',
+          'content' => 'Nous recherchons un développeur Symfony2 débutant sur Lyon. Blabla…',
+          'date'    => new \Datetime()
+        );
 
         return $this->render('PlatformBundle:Advert:view.html.twig', array(
-            'id'  => $id,
+          'advert' => $advert
         ));
     }
 
@@ -57,6 +87,7 @@ class AdvertController extends Controller
     {
         // Ici, on récupérera l'annonce correspondante à $id
 
+
         // Même mécanisme que pour l'ajout
         if ($request->isMethod('POST')) {
             $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
@@ -64,7 +95,17 @@ class AdvertController extends Controller
             return $this->redirectToRoute('platform_view', array('id' => 5));
         }
 
-        return $this->render('PlatformBundle:Advert:edit.html.twig');
+        $advert = array(
+          'title'   => 'Recherche développpeur Symfony',
+          'id'      => $id,
+          'author'  => 'Alexandre',
+          'content' => 'Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…',
+          'date'    => new \Datetime()
+        );
+
+        return $this->render('PlatformBundle:Advert:edit.html.twig', array(
+          'advert' => $advert
+        ));
 
     }
 
@@ -76,5 +117,22 @@ class AdvertController extends Controller
         // Ici, on gérera la suppression de l'annonce en question
 
         return $this->render('PlatformBundle:Advert:delete.html.twig');
+    }
+
+    public function menuAction($limit)
+    {
+        // On fixe en dur une liste ici, bien entendu par la suite
+        // on la récupérera depuis la BDD !
+        $listAdverts = array(
+            array('id' => 2, 'title' => 'Recherche développeur Symfony'),
+            array('id' => 5, 'title' => 'Mission de webmaster'),
+            array('id' => 9, 'title' => 'Offre de stage webdesigner')
+        );
+
+        return $this->render('PlatformBundle:Advert:menu.html.twig', array(
+            // Tout l'intérêt est ici : le contrôleur passe
+            // les variables nécessaires au template !
+            'listAdverts' => $listAdverts
+        ));
     }
 }
